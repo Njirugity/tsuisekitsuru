d50788a99564a4be8662835c8148c26152537510
brian <muriithibrian283@gmail.com>
2025-10-04 13:56:51 +0300

    Create company model repository

---
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/repositories/CompanyRepository.java
@@ -0,0 +1,7 @@
+package ke.tsuisekitsuru.tsuisekitsuru.repositories;
+
+import ke.tsuisekitsuru.tsuisekitsuru.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
