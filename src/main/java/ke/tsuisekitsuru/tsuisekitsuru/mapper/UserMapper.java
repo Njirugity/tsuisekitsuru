e9283dcef64f10826144cf45a7c07ce8fb880036
brian <muriithibrian283@gmail.com>
2025-10-04 13:49:04 +0300

    Rename the user mapermethods

--- src/main/java/ke/tsuisekitsuru/tsuisekitsuru/mapper/UserMapper.java
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/mapper/UserMapper.java
@@ -1,13 +1,13 @@
 package ke.tsuisekitsuru.tsuisekitsuru.mapper;
 
 import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserCreationDTO;
-import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserRolesDTO;
+import ke.tsuisekitsuru.tsuisekitsuru.dtos.UsersDTO;
 import ke.tsuisekitsuru.tsuisekitsuru.models.Users;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserRolesDTO userToUserRoleDTO(Users users);
+    UsersDTO userToUserRoleDTO(Users users);
     Users userCreationDTOtoUsers(UserCreationDTO userCreationDTO);
     UserCreationDTO usersToUserCreationDTO(Users users);
 }
