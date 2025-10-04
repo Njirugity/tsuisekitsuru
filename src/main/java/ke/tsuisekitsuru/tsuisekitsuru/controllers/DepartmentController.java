b9f68ab18daa5ac0075653b0273989f37e553f02
brian <muriithibrian283@gmail.com>
2025-10-04 13:45:16 +0300

    Function to assign a department and a role

--- src/main/java/ke/tsuisekitsuru/tsuisekitsuru/controllers/DepartmentController.java
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/controllers/DepartmentController.java
@@ -1,6 +1,8 @@
 package ke.tsuisekitsuru.tsuisekitsuru.controllers;
 
+import ke.tsuisekitsuru.tsuisekitsuru.models.Company;
 import ke.tsuisekitsuru.tsuisekitsuru.models.Department;
+import ke.tsuisekitsuru.tsuisekitsuru.repositories.CompanyRepository;
 import ke.tsuisekitsuru.tsuisekitsuru.repositories.DepartmentRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +12,11 @@ import java.util.List;
 @RequestMapping("/department")
 public class DepartmentController {
     private DepartmentRepository departmentRepository;
+    private CompanyRepository companyRepository;
 
-    public DepartmentController(DepartmentRepository departmentRepository) {
+    public DepartmentController(DepartmentRepository departmentRepository, CompanyRepository companyRepository) {
         this.departmentRepository = departmentRepository;
+        this.companyRepository = companyRepository;
     }
 
     @GetMapping
@@ -22,4 +26,16 @@ public class DepartmentController {
     public Department addDepartment(@RequestBody Department newDept){
         return departmentRepository.save(newDept);
     }
+
+    @PatchMapping("/{id}/{compId}/assignCompany")
+    public Department assignCompany (@PathVariable("id") Long id, @PathVariable Long compId){
+        Department existingDept = departmentRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Department not found"));
+        Company existingComp = companyRepository.findById(compId).
+                orElseThrow(() -> new RuntimeException("Company not found"));
+
+        existingDept.setCompany(existingComp);
+        return departmentRepository.save(existingDept);
+    }
+
 }
--- src/main/java/ke/tsuisekitsuru/tsuisekitsuru/controllers/UsersController.java
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/controllers/UsersController.java
@@ -1,8 +1,10 @@
 package ke.tsuisekitsuru.tsuisekitsuru.controllers;
 
 import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserCreationDTO;
-import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserRolesDTO;
+import ke.tsuisekitsuru.tsuisekitsuru.dtos.UsersDTO;
+import ke.tsuisekitsuru.tsuisekitsuru.models.Department;
 import ke.tsuisekitsuru.tsuisekitsuru.models.Users;
+import ke.tsuisekitsuru.tsuisekitsuru.repositories.DepartmentRepository;
 import ke.tsuisekitsuru.tsuisekitsuru.repositories.UsersRepository;
 import ke.tsuisekitsuru.tsuisekitsuru.services.UserService;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +15,27 @@ import java.util.List;
 @RequestMapping("/users")
 public class UsersController {
     private UsersRepository usersRepository;
+    private DepartmentRepository departmentRepository;
     private UserService userService;
-    public UsersController(UsersRepository usersRepository, UserService userService){
+    public UsersController(UsersRepository usersRepository, UserService userService, DepartmentRepository departmentRepository){
         this.usersRepository = usersRepository;
         this.userService = userService;
+        this.departmentRepository = departmentRepository;
     }
     @GetMapping
-    public List<UserRolesDTO> getAllUser(){
+    public List<UsersDTO> getAllUser(){
         return userService.getAll();
     }
     @PostMapping("/register")
-    public UserRolesDTO createUser(@RequestBody UserCreationDTO newUser){
+    public UsersDTO createUser(@RequestBody UserCreationDTO newUser){
         return userService.createUser(newUser);
+    }
+    @PatchMapping("/{id}/{deptId}/assignDepartment")
+    public Users assignDepartment(@PathVariable("id") Long id, @PathVariable Long deptId){
+        return userService.assignDepartment(id, deptId);
+    }
+    @PatchMapping("/{id}/{roleId}/assignRoles")
+    public Users assignRoles(@PathVariable("id") Long id, @PathVariable Long roleId){
+        return userService.assignRole(id, roleId);
     }
 }
