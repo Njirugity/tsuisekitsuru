178d7919a8f91092a0adf12e20308ba836a2d151
brian <muriithibrian283@gmail.com>
2025-10-07 20:59:30 +0300

    Add methods tha will change a users attendance status and retrieve all the records

--- src/main/java/ke/tsuisekitsuru/tsuisekitsuru/controllers/DatesController.java
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/controllers/DatesController.java
@@ -2,9 +2,17 @@ package ke.tsuisekitsuru.tsuisekitsuru.controllers;
 
 import ke.tsuisekitsuru.tsuisekitsuru.dtos.DatesRequestDTO;
 import ke.tsuisekitsuru.tsuisekitsuru.dtos.GeneratedDatesDTO;
+import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserAttendanceRequestDTO;
+import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserAttendanceResponseDTO;
 import ke.tsuisekitsuru.tsuisekitsuru.mapper.GeneratedDatesMapper;
+import ke.tsuisekitsuru.tsuisekitsuru.mapper.UserAttendanceResponseMapper;
 import ke.tsuisekitsuru.tsuisekitsuru.models.Dates;
+import ke.tsuisekitsuru.tsuisekitsuru.models.UserAttendance;
+import ke.tsuisekitsuru.tsuisekitsuru.models.Users;
 import ke.tsuisekitsuru.tsuisekitsuru.repositories.DatesRepository;
+import ke.tsuisekitsuru.tsuisekitsuru.repositories.UserAttendanceRepository;
+import ke.tsuisekitsuru.tsuisekitsuru.repositories.UsersRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -15,11 +23,20 @@ import java.util.List;
 @RequestMapping("/dates")
 public class DatesController {
     private DatesRepository datesRepository;
+    private UserAttendanceRepository userAttendanceRepository;
+    private UsersRepository usersRepository;
     private GeneratedDatesMapper generatedDatesMapper;
+    private UserAttendanceResponseMapper userAttendanceResponseMapper;
 
-    public DatesController(DatesRepository datesRepository, GeneratedDatesMapper generatedDatesMapper) {
+    public DatesController(
+            DatesRepository datesRepository, GeneratedDatesMapper generatedDatesMapper,
+            UserAttendanceRepository userAttendanceRepository, UsersRepository usersRepository,
+            UserAttendanceResponseMapper userAttendanceResponseMapper) {
         this.datesRepository = datesRepository;
         this.generatedDatesMapper = generatedDatesMapper;
+        this.userAttendanceRepository = userAttendanceRepository;
+        this.usersRepository = usersRepository;
+        this.userAttendanceResponseMapper = userAttendanceResponseMapper;
     }
 
     @GetMapping
@@ -39,5 +56,25 @@ public class DatesController {
 
         List<Dates> saved = datesRepository.saveAll(savedDates);
         return generatedDatesMapper.datesToDTO(saved);
+    }
+    @PostMapping("/markAttendance")
+    public ResponseEntity<?> markAttendance (@RequestBody UserAttendanceRequestDTO request){
+        Users user = usersRepository.findById(request.getUserId()).
+                orElseThrow(()-> new RuntimeException("User not found"));
+        Dates date = datesRepository.findById(request.getAttendanceId()).
+                orElseThrow(()-> new RuntimeException("Date not found"));
+
+        UserAttendance updateRecord = new UserAttendance();
+        updateRecord.setUsers(user);
+        updateRecord.setDates(date);
+        updateRecord.setStatus(request.isStatus());
+        userAttendanceRepository.save(updateRecord);
+        return ResponseEntity.ok("Attendance marked successfully");
+    }
+
+    @GetMapping("/attendanceRecord")
+    public List<UserAttendanceResponseDTO> getAllAttendance(){
+        List<UserAttendance> attendanceRecord = userAttendanceRepository.findAll();
+        return userAttendanceResponseMapper.userAttendanceToDTOList(attendanceRecord);
     }
 }
