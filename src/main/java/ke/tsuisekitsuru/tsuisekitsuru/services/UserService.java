905ecf1abbd50b194cd136cda251c0b5544e4011
brian <muriithibrian283@gmail.com>
2025-10-09 20:42:44 +0300

    Add methods to update users delete users and retrieve single user

--- src/main/java/ke/tsuisekitsuru/tsuisekitsuru/services/UserService.java
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/services/UserService.java
@@ -1,7 +1,9 @@
 package ke.tsuisekitsuru.tsuisekitsuru.services;
 
+import ke.tsuisekitsuru.tsuisekitsuru.dtos.UpdateUserDTO;
 import ke.tsuisekitsuru.tsuisekitsuru.dtos.UserCreationDTO;
 import ke.tsuisekitsuru.tsuisekitsuru.dtos.UsersDTO;
+import ke.tsuisekitsuru.tsuisekitsuru.mapper.UpdateUserMapper;
 import ke.tsuisekitsuru.tsuisekitsuru.mapper.UserMapper;
 import ke.tsuisekitsuru.tsuisekitsuru.models.Department;
 import ke.tsuisekitsuru.tsuisekitsuru.models.Roles;
@@ -10,43 +12,32 @@ import ke.tsuisekitsuru.tsuisekitsuru.repositories.DepartmentRepository;
 import ke.tsuisekitsuru.tsuisekitsuru.repositories.RolesRepository;
 import ke.tsuisekitsuru.tsuisekitsuru.repositories.UsersRepository;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @Service
 public class UserService {
-    private UsersRepository usersRepository;
-    private DepartmentRepository departmentRepository;
-    private RolesRepository rolesRepository;
-    private UserMapper userMapper;
-    public UserService(UsersRepository usersRepository, UserMapper userMapper, DepartmentRepository departmentRepository, RolesRepository rolesRepository) {
+
+    private final UsersRepository usersRepository;
+    private final DepartmentRepository departmentRepository;
+    private final RolesRepository rolesRepository;
+    private final UserMapper userMapper;
+    private final UpdateUserMapper updateUserMapper;
+
+    public UserService(UsersRepository usersRepository, UserMapper userMapper,
+                       DepartmentRepository departmentRepository, RolesRepository rolesRepository,
+                       UpdateUserMapper updateUserMapper) {
         this.usersRepository = usersRepository;
         this.userMapper = userMapper;
         this.departmentRepository = departmentRepository;
         this.rolesRepository = rolesRepository;
+        this.updateUserMapper = updateUserMapper;
     }
 
-    public UsersRepository getUsersRepository() {
-        return usersRepository;
-    }
-
-    public void setUsersRepository(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
-
-    public UserMapper getUserMapper() {
-        return userMapper;
-    }
-
-    public void setUserMapper(UserMapper userMapper) {
-        this.userMapper = userMapper;
-    }
-
-    public UsersDTO getToUserDTO(Users users){
-        return userMapper.userToUserRoleDTO(users);
-    }
     public UsersDTO createUser(UserCreationDTO userCreationDTO){
         Users newUser = userMapper.userCreationDTOtoUsers(userCreationDTO);
         Users savedUser = usersRepository.save(newUser);
@@ -58,12 +49,35 @@ public class UserService {
      * @return a list of all users
      */
     public List<UsersDTO> getAll(){
-        return usersRepository.findAll()//returns a list of user
-                .stream()//convert this list to in a way that it can be processed
-                .map(this::getToUserDTO)//apply the function to each item in the list
-                .collect(Collectors.toList());//place the dtos in a list
+        return userMapper.userToUserRoleDTOList(usersRepository.findAll());
     }
+    public UpdateUserDTO getUser(Long id){
+        Users existingUsers = usersRepository.findById(id).
+                orElseThrow(()-> new RuntimeException("User not found"));
+        return updateUserMapper.entityToUpdateDTO(existingUsers);
+    }
+    public ResponseEntity <?> updateUserDetails(Long id, UpdateUserDTO userDetails){
+        Users existingUsers = usersRepository.findById(id).
+                orElseThrow(()-> new RuntimeException("User not found"));
 
+        if (userDetails.getName() != null){
+            existingUsers.setName(userDetails.getName());
+        }
+        if(userDetails.getEmail() != null){
+            existingUsers.setEmail(userDetails.getEmail());
+        }
+        if (userDetails.getIdNumber() != null){
+            existingUsers.setIdNumber(userDetails.getIdNumber());
+        }
+        if (userDetails.getPhoneNumber() != null){
+            existingUsers.setPhoneNumber(userDetails.getPhoneNumber());
+        }
+
+        usersRepository.save(existingUsers);
+        Map <String, String> response = new HashMap<>();
+        response.put("message","Update successful");
+        return ResponseEntity.ok(response);
+    }
     public  Users assignRole(Long id, Long addRole){
         Users existingUser = usersRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -83,4 +97,12 @@ public class UserService {
         return usersRepository.save(existingUser);
     }
 
+    public ResponseEntity<?> deleteUser(Long id){
+        Users existingUsers = usersRepository.findById(id).
+                orElseThrow(()-> new RuntimeException("User not found"));
+        usersRepository.delete(existingUsers);
+        Map<String, String> response = new HashMap<>();
+        response.put("message",String.format("User %s successfully removed", existingUsers.getName()));
+        return ResponseEntity.ok(response);
+    }
 }
