d64874a0b0b6a0c26e75c9c9451f01797e08f57a
brian <muriithibrian283@gmail.com>
2025-11-22 12:34:37 +0300

    Add: base class to tract entity creation
    Super class that will add createdAt and modifiedAt fields to enitites that inherit it

--- src/main/java/ke/tsuisekitsuru/tsuisekitsuru/TsuisekitsuruApplication.java
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/TsuisekitsuruApplication.java
@@ -2,8 +2,10 @@ package ke.tsuisekitsuru.tsuisekitsuru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TsuisekitsuruApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ke/tsuisekitsuru/tsuisekitsuru/models/Auditable.java
@@ -0,0 +1,36 @@
+package ke.tsuisekitsuru.tsuisekitsuru.models;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime modifiedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
